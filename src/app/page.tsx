ea5d5a266abe4f551496a4a710ffec8f74204b8f
Babithkp <babithkullachetti44@gmail.com>
2024-06-23 19:50:28 +0530

    half work complated of clients

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 import Image from "next/image";
-import image from "../../public/heroPageImage.png";
+import image from "../../public/backgroundHeroPage.jpg";
 import Link from "next/link";
 import icon1 from "../../public/city.svg";
 import icon2 from "../../public/rocket-graph.svg";
 import icon3 from "../../public/carnival.svg";
 import icon4 from "../../public/flag-point.svg";
-import partnerClg1 from "../../public/logo-vut-big.png";
-import partnerClg2 from "../../public/logo-muni-big-new.png";
-import partnerClg3 from "../../public/logo-mendelu-big.png";
-import cardBg from "../../public/homepage-love-brno.webp";
+import cardBg from "../../public/backgroung-card.png";
 import img1 from "../../public/universitiesLogo/bccc.png";
 import img2 from "../../public/universitiesLogo/maryland.jpg";
 import img3 from "../../public/universitiesLogo/universityOfBaltimorejpg.jpg";
@@ -27,26 +24,23 @@ export default function Home() {
       <Image
         src={image}
         alt="hero image"
-        className="relative h-[130vh] w-full max-sm:h-[100vh] object-cover"
+        className="relative h-[130vh] w-full object-cover max-sm:h-[100vh]"
       />
-      <section className="absolute top-[25%] flex w-[50%]  flex-col items-center justify-center max-md:w-[80%]">
+      <section className="absolute top-[25%] flex w-[50%] flex-col items-center justify-center max-md:w-[80%]">
         <article className="my-10 text-white">
           <h1 className="bg-red-500 px-14 py-10 text-5xl font-semibold max-md:p-4 max-md:text-2xl">
             Study in Baltimore - home to diverse and a vibrant student community
             in Baltimore MD, United States.
           </h1>
           <p className="bg-blue-600 p-8 text-lg font-medium max-md:p-4 max-sm:text-base">
-            We make it possible for international students to get scholarships
-            to study in the city of baltimore, which provides them access world
-            class education and career opportunities.
+            We enable international students to study in Baltimore through
+            various scholarships, granting them free access to world-class
+            education and career opportunities in the United States.
           </p>
         </article>
-        <div className="flex gap-5 max-sm:flex-col">
+        <div className="flex max-sm:flex-col">
           <button className="rounded-md bg-white px-9 py-3 font-semibold text-red-500">
             DISCOVER baltimore
-          </button>
-          <button className="px-5 py-3 text-white underline">
-            DISCOVER CZECHIA
           </button>
         </div>
       </section>
@@ -56,10 +50,9 @@ export default function Home() {
             Study at one of our partner institutions
           </h2>
           <p className="px-5 text-center text-lg font-extralight">
-            Each year we provide the opportunity for over 8000 international
-            student to study a postgraduate or doctoral program in the city of
-            Baltimore on fully funded scholarships at one of our
-            partner universities.
+            Each year we provide scholarship opportunities for more than 1000
+            international students from all over world to study a postgraduate
+            or doctoral degree program at one of our partner universities.
           </p>
           <div className="my-10 flex w-full gap-5 text-center max-lg:flex-wrap max-sm:grid max-sm:grid-cols-2">
             <figure className="flex flex-col items-center justify-center max-sm:w-full">
@@ -117,27 +110,43 @@ export default function Home() {
           </div>
         </div>
         <div className="my-20 flex w-full flex-col flex-wrap items-center justify-center gap-3">
-          <div className="flex flex-col items-center" >
-            <span className="text-lg font-medium uppercase text-red-600">
+          <div className="flex flex-col items-center">
+            <span className="text-lg font-bold uppercase text-red-600 underline">
               what we do
             </span>
-            <h3 className="my-8 w-[80%] text-center text-5xl font-bold text-red-500 max-md:text-3xl">
-              Don&apos;t just fill forms without knowing where to apply
+            <h3 className="my-8 w-[80%] text-center text-4xl font-bold text-red-500 max-md:text-2xl">
+              Direct Entry To Our Prestigious Partner Universities
             </h3>
             <p className="text-center">
-              At Education Street, our mission is to match you with the perfect
-              college for the best fit.
+              Our mission is to make your dream of studying in
+              the USA come true.
             </p>
           </div>
-          <div className="flex gap-5 flex-wrap justify-center items-center">
-          <CardApply image={coachingIcon} title="Scholarship Assistance" content="We are with you even before the admission process begins. Prepare for entrance exams with our help to score better." />
-          <CardApply image={counsellingIcon} title="Admission Guidance" content="We will suggest the right universities and courses, making a personal plan to help you build your career." />
-          <CardApply image={visaIcon} title="Visa Counselling" content="With our personalized visa counselling services, you can forget the stress of filling forms and participating in interviews." />
-          <CardApply image={financialIcon} title="Free Counseling" content="Don’t worry about finances. We’ll demystify the process of creating your budget and get you all the needed assistance." />
+          <div className="flex flex-wrap items-center justify-center gap-5">
+            <CardApply
+              image={coachingIcon}
+              title="Scholarship Assistance"
+              content="Free access to apply for Maryland state-funded scholarships on our application portal."
+            />
+            <CardApply
+              image={counsellingIcon}
+              title="University Placement"
+              content="Submit your application directly to six of our partner universities using our application portal."
+            />
+            <CardApply
+              image={visaIcon}
+              title="Visa Counselling"
+              content="Get professional  student visa counseling and guidance from professionals."
+            />
+            <CardApply
+              image={financialIcon}
+              title="Free Counseling"
+              content="We guide you throughout the entire process of achieving your goals and dreams free of charge!"
+            />
           </div>
         </div>
         <Link href="/" className="mb-10 text-lg font-medium underline">
-          SHOW ALL STUDY PROGRAMMES (197)
+          View Our Top Study Programs.
         </Link>
       </section>
       <section className="mt-5 flex justify-center">
@@ -154,35 +163,14 @@ export default function Home() {
               className="col-span-1 justify-self-center"
             />
             <h3 className="my-5 text-3xl font-bold text-red-500 max-md:text-2xl">
-              World-class Education
+              Free World-Class Education!
             </h3>
             <p className="col-start-2">
-              62,000 students come here every semester, so the city is always
-              alive, teeming with opportunities, events and everything a student
-              needs. That is why it was voted as a 6th most popular student city
-              worldwide!
-            </p>
-            <Link
-              href={"/"}
-              className="col-start-2 text-lg font-medium text-red-500 underline"
-            >
-              MORE INFO
-            </Link>
-          </div>
-          <div className="grid grid-cols-[2rem,1fr] items-center gap-3">
-            <Image
-              src={icon2}
-              alt="city"
-              className="col-span-1 justify-self-center"
-            />
-            <h3 className="my-5 text-3xl font-bold text-red-500 max-md:text-2xl">
-              Best Price–Quality ratio
-            </h3>
-            <p className="col-start-2">
-              There is no need to compromise. baltimore offers very reasonable
-              prices of accommodation, food and public transport while
-              maintaining the world-class quality of education, state-of-the-art
-              facilities and innovative approach.
+              Through our organization&apos;s partnership with leading public
+              universities in Maryland, we offer international students the
+              opportunity to pursue Master&apos;s and PhD degree programs with
+              fully funded scholarships provided by the Maryland State
+              Department of Education.
             </p>
             <Link
               href={"/"}
@@ -198,7 +186,7 @@ export default function Home() {
               className="col-span-1 justify-self-center"
             />
             <h3 className="my-5 text-3xl font-bold text-red-500 max-md:text-2xl">
-            Access to free education! Seamless Application Procedure
+              Access to free education!
             </h3>
             <p className="col-start-2">
               In baltimore, you can explore beautiful historical monuments,
@@ -216,12 +204,35 @@ export default function Home() {
           </div>
           <div className="grid grid-cols-[2rem,1fr] items-center gap-3">
             <Image
+              src={icon2}
+              alt="city"
+              className="col-span-1 justify-self-center"
+            />
+            <h3 className="my-5 text-3xl font-bold text-red-500 max-md:text-2xl">
+              Seamless Application Procedure
+            </h3>
+            <p className="col-start-2">
+              There is no need to compromise. baltimore offers very reasonable
+              prices of accommodation, food and public transport while
+              maintaining the world-class quality of education, state-of-the-art
+              facilities and innovative approach.
+            </p>
+            <Link
+              href={"/"}
+              className="col-start-2 text-lg font-medium text-red-500 underline"
+            >
+              MORE INFO
+            </Link>
+          </div>
+
+          <div className="grid grid-cols-[2rem,1fr] items-center gap-3">
+            <Image
               src={icon4}
               alt="city"
               className="col-span-1 justify-self-center"
             />
             <h3 className="my-5 text-3xl font-bold text-red-500 max-md:text-2xl">
-                Career & Professional networking Opportunities.
+              Career & Professional networking Opportunities.
             </h3>
             <p className="col-start-2">
               The universities are a backbone of the city and are connected to
@@ -242,24 +253,31 @@ export default function Home() {
         <h3 className="my-8 px-5 text-5xl font-bold text-red-500 max-md:text-3xl">
           Find out more about the universities
         </h3>
-        <div className="my-10 flex w-[80%] justify-between flex-wrap max-md:justify-center gap-10">
-          <div className="flex flex-col items-center relative text-center ">
-            <h6 className="text-yellow-300 text-7xl font-semibold">14</h6>
-            <p className="absolute top-[30%] font-bold text-slate-600 text-lg max-md:top-[35%] w-[300%]">YEARS OF EXPERIENCE</p>
+        <div className="my-10 flex w-[80%] flex-wrap justify-between gap-10 max-md:justify-center">
+          <div className="relative flex flex-col items-center text-center">
+            <h6 className="text-7xl font-semibold text-yellow-300">14</h6>
+            <p className="absolute top-[30%] w-[300%] text-lg font-bold text-slate-600 max-md:top-[35%]">
+              YEARS OF EXPERIENCE
+            </p>
           </div>
-          <div className="flex flex-col items-center relative">
-            <h6 className="text-yellow-300 text-7xl font-semibold">18000+</h6>
-            <p className="absolute top-[30%] font-bold text-slate-600 text-lg max-md:top-[35%]">Scholarships Awarded</p>
+          <div className="relative flex flex-col items-center">
+            <h6 className="text-7xl font-semibold text-yellow-300">18000+</h6>
+            <p className="absolute top-[30%] text-lg font-bold text-slate-600 max-md:top-[35%]">
+              Scholarships Awarded
+            </p>
           </div>
-          <div className="flex flex-col items-center relative">
-            <h6 className="text-yellow-300 text-7xl font-semibold">99%</h6>
-            <p className="absolute top-[30%] font-bold text-slate-600 text-lg  max-md:top-[35%] w-[180%]">University Acceptance Rate</p>
+          <div className="relative flex flex-col items-center">
+            <h6 className="text-7xl font-semibold text-yellow-300">99%</h6>
+            <p className="absolute top-[30%] w-[180%] text-lg font-bold text-slate-600 max-md:top-[35%]">
+              University Acceptance Rate
+            </p>
           </div>
-          <div className="flex flex-col items-center relative">
-            <h6 className="text-yellow-300 text-7xl font-semibold">99%</h6>
-            <p className="absolute top-[30%] font-bold text-slate-600 text-lg max-md:top-[35%] w-[120%]">Visa Success Rate</p>
+          <div className="relative flex flex-col items-center">
+            <h6 className="text-7xl font-semibold text-yellow-300">99%</h6>
+            <p className="absolute top-[30%] w-[120%] text-lg font-bold text-slate-600 max-md:top-[35%]">
+              Visa Success Rate
+            </p>
           </div>
-          
         </div>
       </section>
       <section className="flex w-full flex-col items-center justify-center gap-10 py-10">
@@ -268,9 +286,9 @@ export default function Home() {
             <Image
               src={cardBg}
               alt="college background"
-              className="absolute h-full w-full object-cover"
+              className="absolute h-full w-full object-cover blur-[3px]"
             />
-            <h3 className="text-center text-4xl font-bold text-red-500 max-md:text-2xl">
+            <h3 className="text-center text-4xl font-bold text-red-500 max-md:text-2xl z-20">
               There is always someone you can ask
             </h3>
           </div>
